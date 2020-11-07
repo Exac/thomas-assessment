2ed842ca13a440dc2a465f1898d60ae9021c6263
Thomas McLennan <tgmclennan@hotmail.com>
2020-11-07 02:58:35 -0800

    feat: import mongodb into nest

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from '../customer/customer.module';
 
 @Module({
-  imports: [CustomerModule],
+  imports: [
+    CustomerModule,
+    MongooseModule.forRoot('mongodb://mongodb:27017/nest'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
