d6c145bcb89a28a18c1bec7eb60ae6393293b4d7
Thomas McLennan <tgmclennan@hotmail.com>
2020-11-06 16:57:25 -0800

    refactor(angular): prepare unit test
    - Insert a couple customers into the customer database component before each test

--- apps/fake-umbrella/src/app/customer-database/customer-database.component.spec.ts
+++ apps/fake-umbrella/src/app/customer-database/customer-database.component.spec.ts
@@ -1,18 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CustomerDatabaseComponent } from './customer-database.component';
+import {
+  CustomerData,
+  CustomerDatabaseComponent,
+} from './customer-database.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 describe('CustomerDatabaseComponent', () => {
   let component: CustomerDatabaseComponent;
   let fixture: ComponentFixture<CustomerDatabaseComponent>;
+  let customerData: CustomerData[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -35,12 +37,34 @@ describe('CustomerDatabaseComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    customerData = [
+      {
+        company: 'Acme Ltd',
+        contact: 'Alice Adams',
+        employees: 10,
+        id: 0,
+        location: { city: 'Vancouver', state: 'BC' },
+        phone: '778-868-7447',
+      },
+      {
+        company: 'Mind Beacon',
+        contact: 'Bob Bailey',
+        employees: 100,
+        id: 1,
+        location: { city: 'Toronto', state: 'ON' },
+        phone: '416-868-7447',
+      },
+    ];
+    component.customers.data = customerData;
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
   describe('The page should offer the options to create, update, and delete customers', () => {
-    xit('should open an edit dialog when clicking/tapping a customer', () => {
+    it('should open an edit dialog when clicking/tapping a customer', () => {
       // TODO
       // Arrange
       // Act
