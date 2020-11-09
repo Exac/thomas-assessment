6243e59b4e9d108cbdb9bd6bc35ba29608575eb2
Thomas McLennan <tgmclennan@hotmail.com>
2020-11-09 05:15:30 -0800

    WIP

--- apps/fake-umbrella/src/app/top-customers/top-customers.component.ts
+++ apps/fake-umbrella/src/app/top-customers/top-customers.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { CustomerDatabaseService } from '../customer-database.service';
+import { ICustomer } from '@thomas-assessment/api-interfaces';
 
 @Component({
   selector: 'thomas-assessment-top-customers',
   templateUrl: './top-customers.component.html',
-  styleUrls: ['./top-customers.component.css']
+  styleUrls: ['./top-customers.component.css'],
 })
-export class TopCustomersComponent implements OnInit {
+export class TopCustomersComponent implements OnInit, AfterViewInit {
+  public company = [
+    { rain: false, height: 1 },
+    { rain: false, height: 1 },
+    { rain: false, height: 1 },
+    { rain: false, height: 1 },
+  ];
 
-  constructor() { }
+  public maxEmployees: number = 1000;
 
-  ngOnInit(): void {
+  public customers: ICustomer[] = [];
+
+  constructor(private customerDbService: CustomerDatabaseService) {}
+
+  async ngOnInit() {
+    this.customers = await this.customerDbService.findAll().toPromise();
   }
 
+  ngAfterViewInit() {}
 }
